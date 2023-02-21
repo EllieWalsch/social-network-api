ee23cf6e2bbfdfba7def5beac44ea989df31504a
EllieWalsch <elliewalsch4@gmail.com>
2023-02-21 16:48:11 -0600

    Add reactionCount virtual

--- app/thought/Thought.js
+++ app/thought/Thought.js
@@ -27,6 +27,8 @@ const thoughtSchema = new Schema(
   }
 );
 
-// TODO: Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
 export default model("Thought", thoughtSchema);
