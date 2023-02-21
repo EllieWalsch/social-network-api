19075747ed34209f106281297154b8dcb8521366
EllieWalsch <elliewalsch4@gmail.com>
2023-02-21 15:45:24 -0600

    Add footnotes

--- app/thought/reaction-schema.js
+++ app/thought/reaction-schema.js
@@ -1,24 +1,31 @@
 import { Schema, Types } from "mongoose";
 
-const reactionSchema = new Schema({
-  reactionID: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
+const reactionSchema = new Schema(
+  {
+    reactionID: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: [true, "Input is required"],
+      maxLength: [280, "Must be under 280 characters long"],
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // TODO: add getter to format the timestamp on query
+    },
   },
-  reactionBody: {
-    type: String,
-    required: [true, "Input is required"],
-    maxLength: [280, "Must be under 280 characters long"],
-  },
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    // TODO: add getter to format the timestamp on query
-  },
-});
+  {
+    strict: "throw",
+    toJSON: { getters: true, virtuals: true },
+    id: false,
+  }
+);
 
 export default reactionSchema;
