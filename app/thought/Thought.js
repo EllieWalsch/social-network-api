dfd541c34908a3797a53aa47bef8d48af1240fd3
EllieWalsch <elliewalsch4@gmail.com>
2023-02-21 15:49:03 -0600

    Add footnotes

--- app/thought/Thought.js
+++ app/thought/Thought.js
@@ -1,22 +1,29 @@
 import { Schema } from "mongoose";
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: [true, "Text required"],
-    minLength: [1, "Must be at least 1 character long"],
-    maxLength: [280, "Must be under 280 characters long"],
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: [true, "Text required"],
+      minLength: [1, "Must be at least 1 character long"],
+      maxLength: [280, "Must be under 280 characters long"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // TODO: add getter to format the timestamp on query
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    // TODO: add reactions
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    // TODO: add getter to format the timestamp on query
-  },
-  username: {
-    type: String,
-    required: [true, "Username is required"],
-  },
-  // TODO: add reactions
-});
+  {
+    strict: "throw",
+    toJSON: { virtuals: true, getters: true },
+    id: false,
+  }
+);
 
 export default thoughtSchema;
