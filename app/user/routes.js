8cdb5caf741256e1d167cbb7ec6077d1b953897f
EllieWalsch <elliewalsch4@gmail.com>
2023-02-22 10:23:02 -0600

    Get one user

--- app/user/routes.js
+++ app/user/routes.js
@@ -4,9 +4,25 @@ import userController from "./controller.js";
 const router = Router();
 
 router.get("/", (_, res) => {
-  userController.findAll().then((users) => {
-    res.json(users);
-  });
+  userController
+    .findAll()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.json({ error: err.message });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  userController
+    .findOne(req.params.id)
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      res.json({ error: err.message });
+    });
 });
 
 export default router;
