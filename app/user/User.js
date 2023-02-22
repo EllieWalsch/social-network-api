2301185e4e59269213637eb4a1602fef79318d0c
EllieWalsch <elliewalsch4@gmail.com>
2023-02-22 08:52:45 -0600

    Add friendCount virtual

--- app/user/User.js
+++ app/user/User.js
@@ -43,6 +43,8 @@ const userSchema = new Schema({
   ],
 });
 
-// TODO: Create a virtual called friendCount that retrieves the length of the user's friends array field on query
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
 export default model("User", userSchema);
