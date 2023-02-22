ba1aa1e243fa44cca4dbf271970da508aaa56350
EllieWalsch <elliewalsch4@gmail.com>
2023-02-22 08:40:33 -0600

    Fix unique validator

--- app/user/User.js
+++ app/user/User.js
@@ -1,19 +1,37 @@
-import { model, Schema } from "mongoose";
+import mongoose, { model, Schema } from "mongoose";
 
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: true,
     required: [true, "Username is required"],
     trim: true,
+    validate: {
+      async validator(username) {
+        const duplicate = await mongoose.models.Student.findOne({ username });
+
+        // inverse the boolean value if duplicate is found
+        return !duplicate;
+      },
+      message: "Duplicate username",
+    },
   },
   email: {
     type: String,
-    unique: true,
     required: [true, "Email is required"],
+    validate: {
+      async validator(email) {
+        const duplicate = await mongoose.models.Student.findOne({ email });
+
+        // Inverse the boolean value if duplicate is found
+        return !duplicate;
+      },
+      message: "Duplicate email",
+    },
     match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, "Invalid email"],
   },
   // TODO: add thoughts and friends
 });
+
+// TODO: Create a virtual called friendCount that retrieves the length of the user's friends array field on query
 
 export default model("User", userSchema);
