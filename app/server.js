4b779ad6a97866d87a05a3c5f92f2e0c62c498bd
EllieWalsch <elliewalsch4@gmail.com>
2023-02-22 10:11:11 -0600

    Use userRoutes

--- app/server.js
+++ app/server.js
@@ -1,10 +1,13 @@
 import express from "express";
+import userRoutes from "./user/routes.js";
 
 const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/users", userRoutes);
 
 export default () => {
   app.listen(PORT, () => {
