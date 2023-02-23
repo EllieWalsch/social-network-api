6a1f86c0fe764b6f2077a1ca21babf4cad123734
EllieWalsch <elliewalsch4@gmail.com>
2023-02-23 11:11:34 -0600

    Use thought routes

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import thoughtRoutes from "./thought/routes.js";
 import userRoutes from "./user/routes.js";
 
 const PORT = 3000;
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use("/api/thoughts", thoughtRoutes);
 
 export default () => {
   app.listen(PORT, () => {
